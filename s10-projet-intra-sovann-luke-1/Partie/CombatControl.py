21c78c6a2e8224e1d66418d06361b130095bab70
luke <6333330@cegepoutaouais.qc.ca>
2026-05-06 21:50:55 -0400

    Commentaires + quelques explications

--- s10-projet-intra-sovann-luke-1/Partie/CombatControl.py
+++ s10-projet-intra-sovann-luke-1/Partie/CombatControl.py
@@ -22,29 +22,29 @@ class CombatController:
         self.combat = None # Contiendra l’objet Combat une fois le combat créé
         self.tour = 1 # Numéro du tour actuel
         self.ordre = [] # Liste de personnages qui vont jouer dans l'ordre du tour
-        self.index = 0 # Position
+        self.index = 0 # Position du personnage actuel
 
-        self.action = None
-        self.cible = None
+        self.action = None # Action choisie par le joueur.
+        self.cible = None # Cible de l'action
 
     def init_combat(self):
-        if not (0 < len(self.dict_heros) <= 4):
+        if not (0 < len(self.dict_heros) <= 4): # Minimum 1 héro. Maximum 4 héros.
             raise ValueError("Pas assez de héros")
 
         self.combat = Combat([], [], "FORET")
         self.combat.inserer_heros(self.dict_heros, self.dict_type_hero)
         self.combat.generer_ennemis(self.dict_scene, self.dict_ennemi)
 
-        self.next_turn()
+        self.next_turn() # Démarrage du premier tour
 
-    def next_turn(self):
+    def next_turn(self): # Prépare un nouveau tour
         self.ordre = self.combat.evaluation_agilite_personnage()
-        self.index = 0
+        self.index = 0 # Le 1er personnage peut jouer
 
-    def current(self):
+    def personnage_actuel(self):
         return self.ordre[self.index]
 
-    def next_personnage(self):
+    def prochain_personnage(self):
         self.index += 1
 
         if self.index >= len(self.ordre):
@@ -67,12 +67,12 @@ class CombatController:
                     cible.stats["point_vie"] = 0
 
     def do_action(self, cible):
-        perso = self.current()
+        perso = self.personnage_actuel()
 
         action = Action(perso, self.action, cible)
         action.verification_type_actions(self.dict_abilite, self.combat)
 
         self.action = None
-        self.next_personnage()
+        self.prochain_personnage()
 
     
